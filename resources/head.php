e434dc0696f1dc735080aa9ff5df7cd11b2add35
Mario Kunz <mario.kunz@bbcag.ch>
2015-05-23 16:14:37 +0200

    saturday

--- resources/head.php
+++ resources/head.php
@@ -2,19 +2,16 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <link href='http://fonts.googleapis.com/css?family=Sigmar+One' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,700' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" type="image/ico" href="http://localhost/inf-stube14-tasktool/favicon.ico" />
     <link href='http://fonts.googleapis.com/css?family=Play:400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="public/css/bootstrap.min.css">
-    <link rel="stylesheet" href="public/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="public/css/style.css">
-    <script src="public/js/jquery-2.1.3.min.js"></script>
+    <script src="public/js/jquery-1.11.3.min.js"></script>
     <script src="public/js/jquery-ui.min.js"></script>
     <script src="public/js/bootstrap.min.js"></script>
     <script src="public/js/jquery.js"></script>
     <script src="public/js/validate.js"></script>
-    <script src="public/js/authentificationfails.js"></script>
+    <script src="public/js/changesite.js"></script>
     <script>
         $(document).ready(function(){
             $('[data-toggle="popover"]').popover();

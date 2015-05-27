c96bbc2331389278670574a3e0c22182e8bc5aaf
Mario Kunz <mario.kunz@bbcag.ch>
2015-05-27 07:47:57 +0200

    Join works finally

--- lib/DisplayTasks.php
+++ lib/DisplayTasks.php
@@ -5,12 +5,17 @@ require_once('lib/Connectionhandler.php');
 Class DisplayTasks
 {
 
-    public function sortByPriority()
+    public function sortByPriorityAsc()
     {
-
-        $query = 'SELECT t.description, s.name, tt.type, t.enddate, t.priority FROM task AS LEFT JOIN subject AS s ON t.subject_id = s.id_subject LEFT JOIN task_type AS tt ON t.task_type_id = tt.id_task_type ORDER BY t.priority ASC';
+        $query = 'SELECT t.description, s.name, tt.type, t.enddate, t.priority FROM task AS t
+LEFT JOIN subject AS s ON t.subject_id = s.id_subject
+LEFT JOIN task_type AS tt ON t.task_type_id = tt.id_task_type
+INNER JOIN user_task AS ut ON t.id_task = ut.task_id
+WHERE ut.user_id = ?
+ORDER BY t.priority ASC';
 
         $statement = ConnectionHandler::getConnection()->prepare($query);
+        $statement->bind_param('i', $_SESSION['user-id']);
 
         $statement->execute();
 

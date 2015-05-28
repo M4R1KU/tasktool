602a15f8c9775c78f2bcb8e876531a0e09ce3ac7
Mario Kunz <mario.kunz@bbcag.ch>
2015-05-28 17:23:20 +0200

    Version 1.02

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 return array(
 
     'database' => array(
-        'host'     => 'localhost:3307',
-        'username' => 'mamiadmin',
+        'host'     => 'localhost:3306',
+        'username' => 'bkunzmsql1',
         'password' => 'Welcome$15',
-        'database' => 'tasktool',
+        'database' => 'bkunzmsql1',
     ),
 
 );
